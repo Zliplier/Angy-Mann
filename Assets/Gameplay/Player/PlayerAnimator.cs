7b312f2397faf25bb722d11bf0f4fd886ae2402a
Zliplier <ktiranakul@gmail.com>
2026-08-31 08:53:04 +0700

    Init

---
+++ Assets/Gameplay/Player/PlayerAnimator.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Gameplay.Player
+{
+    public class PlayerAnimator : MonoBehaviour
+    {
+        [Header("Components")]
+        [SerializeField] private Animator animator;
+        
+        
+        
+    }
+}
